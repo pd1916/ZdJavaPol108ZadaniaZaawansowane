6b1f47c6be141707c12406d8c9e7274a54356337
Pawel Dudko <paweldudko@outlook.com>
2022-04-24 16:07:26 +0200

    solution day1_task9_task10

---
+++ src/main/java/pl/sdacademy/java/advance/exercises/day1/task9/Movable.java
@@ -0,0 +1,5 @@
+package pl.sdacademy.java.advance.exercises.day1.task9;
+
+public interface Movable {
+    void move(MoveDirection moveDirection);
+}
---
+++ src/main/java/pl/sdacademy/java/advance/exercises/day1/task9/Point2D.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.java.advance.exercises.day1.task9;
+
+public class Point2D implements Movable{
+    private double x;
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public void move(MoveDirection moveDirection) {
+        x += moveDirection.getX();
+        y += moveDirection.getY();
+    }
+}
